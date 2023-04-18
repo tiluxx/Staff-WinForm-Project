f2881f1a17b6307cb4fa8cc2d08e55db1bb3b7ed
TiLux257 <work.lethanhtien@gmail.com>
2023-04-18 14:44:45 +0700

    Add WarehouseReceiptDTO class.

---
+++ DTO/WarehouseReceiptDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class WarehouseReceiptDTO
+    {
+        private string productID, productName, productSize, productUnitSize, productBrand, productOrigin;
+        private int productQuantity;
+        private bool productDeleted;
+        private decimal productPrice;
+    }
+}
