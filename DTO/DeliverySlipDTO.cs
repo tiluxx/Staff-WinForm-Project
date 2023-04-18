833e67f6a53321c8b31559e4d1863af5e9265d80
JLBiLong <trantobaolong0802@gmail.com>
2023-04-18 18:38:46 +0700

    Add DelliverySlipDTO class.

---
+++ DTO/DeliverySlipDTO.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    internal class DeliverySlipDTO
+    {
+        private string slipID, orderID;
+        private DateTime deliveryDate;
+        private decimal totalBill;
+        private bool deliverySlipDeleted;
+
+
+        public string GetSlipID
+        {
+            get
+            {
+                return slipID;
+            }
+            set
+            {
+                slipID = value;
+            }
+        }
+
+        public string GetOrderID
+        {
+            get
+            {
+                return orderID;
+            }
+            set
+            {
+                orderID = value;
+            }
+        }
+        public DateTime GetDeliveryDate
+        {
+            get
+            {
+                return deliveryDate;
+            }
+            set
+            {
+                deliveryDate = value;
+            }
+        }
+        public decimal GetTotalBill
+        {
+            get
+            {
+                return totalBill;
+            }
+            set
+            {
+                totalBill = value;
+            }
+        }
+
+        public bool GetDeliverySlipDeleted
+        {
+            get
+            {
+                return deliverySlipDeleted;
+            }
+            set
+            {
+                deliverySlipDeleted = value;
+            }
+        }
+
+
+        public DeliverySlipDTO(
+            string SlipID,
+            string OrderID,       
+            DateTime DeliveryDate,
+            bool DeliverySlipDeleted,
+            decimal TotalBill)
+        {
+            this.slipID = SlipID;
+            this.orderID = OrderID;
+            this.deliveryDate = DeliveryDate;
+            this.deliverySlipDeleted = DeliverySlipDeleted;
+            this.totalBill = TotalBill; 
+        }
+    }
+}
--- DTO/OrderDTO.cs
+++ DTO/OrderDTO.cs
@@ -8,5 +8,139 @@ namespace DTO
 {
     internal class OrderDTO
     {
+        private string orderID, agentID, orderStatus, paymentStatus, paymentMethod;
+        private decimal totalBill;
+        private DateTime orderDate, paymentDate;
+        private bool orderDeleted;
+
+        public string GetOrderID
+        {
+            get
+            {
+                return orderID;
+            }
+            set
+            {
+                orderID = value;
+            }
+        }
+
+        public string GetAgentID
+        {
+            get
+            {
+                return agentID;
+            }
+            set
+            {
+                agentID = value;
+            }
+        }
+
+        public string GetOrderStatus
+        {
+            get
+            {
+                return orderStatus;
+            }
+            set
+            {
+                orderStatus = value;
+            }
+        }
+
+        public string GetPaymentStatus
+        {
+            get
+            {
+                return paymentStatus;
+            }
+            set
+            {
+                paymentStatus = value;
+            }
+        }
+
+        public string GetPaymentMethod
+        {
+            get
+            {
+                return paymentMethod;
+            }
+            set
+            {
+                paymentMethod = value;
+            }
+        }
+
+        public decimal GetTotalBill
+        {
+            get
+            {
+                return totalBill;
+            }
+            set
+            {
+                totalBill = value;
+            }
+        }
+
+        public bool CheckOrderDeleted
+        {
+            get
+            {
+                return orderDeleted;
+            }
+            set
+            {
+                orderDeleted = value;
+            }
+        }
+        public DateTime GetOrderDate
+        {
+            get 
+            {
+                return orderDate;
+            }
+            set
+            {
+                orderDate = value;
+            }
+        }
+        public DateTime GetPaymentDate
+        {
+            get
+            {
+                return paymentDate;
+            }
+            set
+            {
+                paymentDate = value;
+            }
+        }
+
+        public OrderDTO(
+          string OrderID,
+          string AgentID,
+          string OrderStatus,
+          string PaymentStatus,
+          string PaymentMethod,
+          bool OrdertDeleted,
+          decimal TotalBill,
+          DateTime OrderDate,
+          DateTime PaymentDate
+          )
+        {
+            this.orderID = OrderID;
+            this.agentID = AgentID;
+            this.orderStatus = OrderStatus;
+            this.paymentStatus = PaymentStatus;
+            this.paymentMethod = PaymentMethod;
+            this.totalBill = TotalBill;
+            this.orderDate = OrderDate;
+            this.paymentDate = PaymentDate;
+            this.orderDeleted = OrdertDeleted;
+        }
     }
 }
+
